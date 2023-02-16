7431f4cd06c88d205c03de3571bc261efab29895
Sathishsooraa2312 <sathishrohin22@gmail.com>
2023-02-16 15:29:14 +0530

    color commit

--- src/components/AddColor.js
+++ src/components/AddColor.js
@@ -5,19 +5,22 @@ import CardContent from '@mui/material/CardContent';
 import { Card } from '@mui/material';
 export function AddColor() {
 
-  const [Colorlist, setColorlist] = useState(["orange", "red", "yellow", "green", "skyblue"]);
+  const [Colorlist, setColorlist] = useState(["orange", "red"]);
   let [color, setColor] = useState("");
   const styles = {
-    backgroundColor: color
+    backgroundColor: color,
+    height:"40px",
+    fontSize:"20px",
+    borderRadius:"20px"
   };
   return <>
   <Card>
 <CardContent className="color-movie-form">
     <input type="text"
       style={styles}
-      placeholder="name"
+      placeholder="Enter Your Color"
       onChange={(event) => setColor(event.target.value)} />
-    <button onClick={() => setColorlist([...Colorlist, color])}>Add Color</button>
+    <button className='color-button' onClick={() => setColorlist([...Colorlist, color])}>Add Color</button>
     <h1>color:{color}</h1>
 
     {Colorlist.map((clr) => (<Colorbox a={clr} />))}
